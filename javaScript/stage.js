b5fe55562395a823665bd93e91464f3b80cc6d5a
silvio <silvio.mattioli@student.odisee.be>
2018-11-27 13:19:49 +0100

    laatste aanpassing omvattend read csv

--- javaScript/stage.js
+++ javaScript/stage.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
                     scrollTop: target.offset().top
                 }, 1000);
                 return false;
-                /*lkmlkl*/
             }
         }
     });
@@ -34,11 +33,6 @@ $(document).ready(function () {
 
                 console.log(csv_to_array(reader.result)); 
 
-
-                var values = reader.result.split(',');
-                console.log(values);
-
-
             };
 
             // start reading the file. When it is done, calls the onload event defined above.
@@ -47,24 +41,6 @@ $(document).ready(function () {
         };
 
     fileInput.addEventListener('change', readFile);
-
-    // $.csv.toArrays(readFile);
-    /*  function processData(allText) {
-    var record_num = 14;  // or however many elements there are in each row
-    var allTextLines = allText.split(/\r\n|\n/);
-    var entries = allTextLines[0].split(',');
-    var lines = [];
-
-    var headings = entries.splice(0,record_num);
-    while (entries.length>0) {
-        var tarr = [];
-        for (var j=0; j<record_num; j++) {
-            tarr.push(headings[j]+":"+entries.shift());
-        }
-        lines.push(tarr);
-    }
-    // alert(lines);
-}*/
 
 
 });
